27dcbd8c3a86ae09ee58183fad07321d00df23dd
kestjohn <132518818+kestjohn@users.noreply.github.com>
2023-07-24 13:34:59 -0700

    added API JS

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ let days = [
 
 date.innerHTML = `${days[day]}: ${hours}:${minutes}`;
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
   let forecastHTML = `<div class="row">`;
@@ -45,6 +45,12 @@ function displayForecast() {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "a6244f5636e152e1c98a09dc4d66a96a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showTemp(response) {
@@ -72,6 +78,7 @@ function showTemp(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   fahrenheitTemp = response.data.main.temp;
+  getForecast(response.data.coord);
 }
 
 function searchPosition(event) {
